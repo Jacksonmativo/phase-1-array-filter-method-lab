447213f8a0fbefa6a56725c131b64e6554f6f093
jackson mativo <jacksonmativo21@gmail.com>
2025-03-19 20:25:51 -0400

    master

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+// Case-insensitive matching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // Matches names that start with the given letters
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  // Matches objects where the name property matches the given string
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
